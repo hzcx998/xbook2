bef3f047e0f6a1f6c5113e8f52896765180c4c42
hzcx998 <2323168280@qq.com>
2020-03-25 23:30:36 +0800

    vmspace

--- src/include/xbook/vmspace.h
+++ src/include/xbook/vmspace.h
@@ -2,6 +2,7 @@
 #define _XBOOK_VMSPACE_H
 
 #include "vmm.h"
+#include "memcache.h"
 
 /* 虚拟内存地址空间描述 */
 typedef struct vmspace {
@@ -14,12 +15,7 @@ typedef struct vmspace {
 } vmspace_t;
 
 
-/* 虚拟空间的插入。
-推演：
-space 0 100
-space 200 300
-space 100 200
- */
+/* 虚拟空间的插入。*/
 static inline vmspace_insert(vmm_t *vmm, vmspace_t *space)
 {
     vmspace_t *prev = NULL, *p = vmm->vmspace_head;
@@ -30,18 +26,59 @@ static inline vmspace_insert(vmm_t *vmm, vmspace_t *space)
         prev = p;
         p = p->next;
     }
-
     /* p是space的后一个 */
     space->next = p;
     if (prev)   /* 把space插入到prev和p中间 */
         prev->next = space;
     else    /* 如果前一个是空，说明插入到队首 */
         vmm->vmspace_head = space;
-    
     space->vmm = vmm; /* 绑定空间的虚拟内存管理 */
     /* 合并相邻的空间 */
-
 }
+
+static inline vmspace_remove(vmm_t *vmm, vmspace_t *space, vmspace_t *prev)
+{
+    if (prev)   /* 没在链表头，位于中间或者后面 */
+        prev->next = space->next;
+    else    /* 前面没有空间，说明在链表头 */
+        vmm->vmspace_head = space->next;    
+    kfree(space);
+}
+
+static inline vmspace_find(vmm_t *vmm, unsigned long addr)
+{
+    vmspace_t *space = vmm->space_head;
+    while (space) {
+        if (addr < space->end)  /* 地址小于某个控件结束位置就返回该空间 */
+            return space;
+        space = space->next;
+    }
+    return NULL;
+}
+
+static inline vmspace_find_prev(vmm_t *vmm, unsigned long addr, vmspace_t *prev)
+{
+    prev = NULL;    /* prev save prev space ptr, set NULL first */
+    vmspace_t *space = vmm->space_head;
+    while (space) {
+        if (addr < space->end)  /* 地址小于某个控件结束位置就返回该空间 */
+            return space;
+        prev = space;   /* save prev */
+        space = space->next;
+    }
+    return NULL;
+}
+
+/* 如果一个空间的开始地址和另一个空间的结束地址位于同一个空间范围，说明二者相交 */
+static inline vmspace_t *vmspace_find_intersection(vmm_t *vmm,
+    unsigned long start, unsigned long end)
+{
+    vmspace_t *space = vmspace_find(vmm, start);
+    if (space && space->start >= end) /* 如果第一个空间的开始大于第二个空间的结束，说明不相交 */
+        space = NULL;
+    return space;
+}
+
 
 
 
