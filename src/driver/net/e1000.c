d91b8b5165c0435a28fe2f55c0ddc1ac599e3c16
huloves <17791627852@163.com>
2020-08-16 19:13:46 +0800

    e1000.c - struct _device_extension{};

--- src/driver/net/e1000.c
+++ src/driver/net/e1000.c
@@ -15,6 +15,7 @@
 #include <arch/atomic.h>
 #include <arch/cpu.h>
 #include <sys/ioctl.h>
+#include <stddef.h>
 
 #include <net/e1000_hw.h>
 #include <net/e1000_osdep.h>
@@ -33,3 +34,46 @@
 #define ETH_ZLEN 60 /*不含CRC校验的数据最小长度*/
 #define ETH_DATA_LEN 1500 /*帧内数据的最大长度*/
 #define ETH_FRAME_LEN 1514 /*不含CRC校验和的最大以太网数据长度*/
+
+/*  设备状态 */
+struct net_device_status {
+    /* 错误记录 */
+    unsigned long tx_errors;         /* 传输错误记录 */
+    unsigned long tx_aborted_errors;  /* 传输故障记录 */
+    unsigned long tx_carrier_errors;  /* 传输携带错误记录 */
+    unsigned long tx_window_errors;   /* 传输窗口错误记录 */
+    unsigned long tx_fifo_errors;     /* 传输fifo错误记录 */
+    unsigned long tx_dropped;        /* 传输丢弃记录 */
+
+    unsigned long rx_errors;         /* 接收错误 */    
+    unsigned long rx_length_errors;   /* 接收长度错误 */
+    unsigned long rx_missed_errors;   /* 接收丢失错误 */
+    unsigned long rx_fifo_errors;     /* 接收Fifo错误 */
+    unsigned long rx_crc_errors;      /* 接收CRC错误 */
+    unsigned long rx_frame_errors;    /* 接收帧错误 */
+    unsigned long rx_dropped;        /* 接收丢弃记录 */
+    /* 正确记录 */
+    unsigned long tx_packets;        /* 传输包数量 */
+    unsigned long tx_bytes;          /* 传输字节数量 */
+    
+    unsigned long collisions;       /* 碰撞次数 */
+};
+
+typedef struct _device_extension {
+    device_object_t *device_object;   //设备对象
+
+    uint32_t io_addr;   //IO基地址
+    int drv_flags;   //驱动标志
+    uint32_t irq;
+    uint8_t mac_addr[6];
+    flags_t flags;
+    pci_device_t *pci_device;
+
+    uint32_t dev_features;   //设备结构特征
+
+    uint8_t *rx_buffer;   //接受缓冲区
+    uint8_t *rx_ring;   //接受环
+    uint8_t current_rx;
+    flags_t rx_flags;
+    dma_addr_t rx_ring_dma;   //dma物理地址
+};
