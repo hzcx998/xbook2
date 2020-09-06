2d04665f4d6b138fe596d1c6e78c946f2e0064b1
hzcx998 <2323168280@qq.com>
2020-09-07 00:17:30 +0800

    fix bosh bug when child get input

--- develop/rom/res/test.lua
+++ develop/rom/res/test.lua
@@ -1,3 +1,4 @@
+--[[
 print(type("Hello world"))      --> string
 print(type(10.4*3))             --> number
 print(type(print))              --> function
@@ -63,3 +64,17 @@ print(coroutine.status(co2))   -- suspended
 print(coroutine.running())
 
 print("----------")
+
+--]]
+
+-- 以附加的方式打开只写文件
+file = io.open("res/wr.lua", "w+")
+
+-- 设置默认输出文件为 test.lua
+io.output(file)
+
+-- 在文件最后一行添加 Lua 注释
+io.write("hello file!\n")
+
+-- 关闭打开的文件
+io.close(file)
--- user/bosh/console/console.c
+++ user/bosh/console/console.c
@@ -705,6 +705,21 @@ int con_xmit_key(int kcode, int kmod)
             return 0;   /* 特殊按键处理 */
         }
     }
+    /* 过滤一些按键 */
+    switch (kcode) {
+    case GK_NUMLOCK:
+    case GK_CAPSLOCK:
+    case GK_SCROLLOCK:
+    case GK_RSHIFT:
+    case GK_LSHIFT:
+    case GK_RCTRL:
+    case GK_LCTRL:
+    case GK_RALT:
+    case GK_LALT:
+        return 0;   /* 特殊按键处理 */
+    default:
+        break;
+    }
     shell_child_key = kcode;
     return 1;
 }
