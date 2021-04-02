5dbb2e3564d196e1f11a79e981ad76bf218b1110
st127 <tswuyin_st127@163.com>
2021-04-02 22:50:23 +0800

    fix: Error message should print to stderr

--- user/bin/rmdir/main.c
+++ user/bin/rmdir/main.c
@@ -6,13 +6,13 @@ int main(int argc, char *argv[])
 {
 	int ret = -1;
 	if(argc != 2){
-		printf("mkdir: no argument support!\n");
+		fprintf(stderr,"mkdir: no argument support!\n");
 	}else{
         if(rmdir(argv[1]) == 0){
             //printf("rmdir: remove %s success.\n", argv[1]);
             ret = 0;
         }else{
-            printf("rmdir: remove %s faild!\n", argv[1]);
+            fprintf(stderr,"rmdir: remove %s faild!\n", argv[1]);
         }
 		
 	}
