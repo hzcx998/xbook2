5904852a311965c3ce6513401608870880d8eaad
hzc1998 <2323168280@qq.com>
2021-04-10 17:30:48 +0800

    fix: bug on env.mk

--- env.mk
+++ env.mk
@@ -1,15 +1,15 @@
 # env var 
 ENV_CFLAGS	:= -march=i386 -fno-builtin -Wall -Wunused -fno-PIE -m32 -std=gnu99 -O3 -fno-stack-protector 
 ENV_AFLAGS	:= -f elf 
-ENV_LDFLAGS	:= -m elf_i386 -no-pie 
+ENV_LDFLAGS	:= -no-pie 
 
 ENV_USER_LD_SCRIPT	:= ../libs/xlibc/arch/x86/user.ld
 
 # MacOS specail
 ifeq ($(shell uname),Darwin)
-	ENV_LD		:=  i386-elf-ld
+	ENV_LD		:=  i386-elf-ld -m elf_i386
 else
-	ENV_LD		:=  ld
+	ENV_LD		:=  ld -m elf_i386
 endif
 
 ENV_AS		:= nasm
