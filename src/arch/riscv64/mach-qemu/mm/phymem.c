d96054cc80471cd5f718876db37ca95a73b98503
hzcx998 <2323168280@qq.com>
2021-05-12 02:18:04 +0800

    feat: calc free memory size

--- src/arch/riscv64/mach-qemu/mm/phymem.c
+++ src/arch/riscv64/mach-qemu/mm/phymem.c
@@ -16,20 +16,14 @@ unsigned long mem_get_total_page_nr()
     return total_pmem_size / PAGE_SIZE;
 }
 
-/**
- * 将物理内存划分为3个区域。
- * DMA区域：专门提供给DMA设备使用，在初始化时已经映射到虚拟地址，所以可以直接把物理地址转换成虚拟地址。
- * NORMAL区域：专门提供给内核使用，在初始化时已经映射到虚拟地址，所以可以直接把物理地址转换成虚拟地址。
- * USER区域：专门提供给用户进程空间使用，由于是在使用过程中才映射的，所以不能直接物理地址转虚拟地址。
- *          只能使用计算公式把虚拟地址转物理地址。
- */
 int physic_memory_init()
 {
     total_pmem_size = PHYSIC_MEM_SIZE;
     u64_t kern_size = (u64_t) (kernel_end - kernel_start);
+    u64_t free_size = total_pmem_size - kern_size - RUSTSBI_MEM_SIZE;
     keprint("memory total size:0x%x Bytes %d MB\n", total_pmem_size, total_pmem_size / MB);
     keprint("kernel image: [0x%p, 0x%p]\n", kernel_start, kernel_end);
     keprint("kernel size: 0x%x Bytes %d MB\n", kern_size, kern_size / MB);
-    
+    keprint("free size: 0x%x Bytes %d MB\n", free_size, free_size / MB);
     return 0;
 }
